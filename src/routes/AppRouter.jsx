1f926383a5fdbec7e4029a4e5797895c434d8c71
XDR-SAM <tdxfarhan@gmail.com>
2025-11-13 17:04:44 +0600

    Add ArtistDetailsGallery route to AppRouter

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -11,6 +11,7 @@ import ArtworkDetails from '../pages/ArtworkDetails';
 import MyGallery from '../pages/MyGallery';
 import MyFavorites from '../pages/MyFavorites';
 import NotFound from '../pages/NotFound';
+import ArtistDetailsGallery from '../pages/ArtistDetailsGallery';
 
 const router = createBrowserRouter([
   {
@@ -36,6 +37,10 @@ const router = createBrowserRouter([
       {
         path: 'explore',
         element: <ExploreArtworks />,
+      },
+      {
+        path: 'artist/:email',
+        element: <ArtistDetailsGallery />,
       },
       {
         path: 'add-artwork',
